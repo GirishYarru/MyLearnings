88f5756808fe7b3aa5fe4cdbad2c35d4b67283b1
M1048219 <girish.yarru@mindtree.com>
2019-11-28 11:47:05 +0530

    Enable hystrix dash board

---
+++ producer-service-discovery/src/main/java/com/eureka/producerservicediscovery/ProducerServiceDiscoveryApplication.java
@@ -0,0 +1,21 @@
+package com.eureka.producerservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrix
+public class ProducerServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ producer-service-discovery/src/main/java/com/eureka/producerservicediscovery/controller/ProducerController.java
@@ -0,0 +1,33 @@
+package com.eureka.producerservicediscovery.controller;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class ProducerController {
+
+	@HystrixCommand(fallbackMethod = "fallbackMethod")
+	@RequestMapping(value = "/printProducerName/{name}")
+	public String printProducerName(@PathVariable(name = "name") String name,
+			@RequestParam(value = "id", required = false) String id) {
+		if (RandomUtils.nextBoolean()) {
+			throw new RuntimeException("Exception Occured");
+		}
+		System.out.println("******* In Actual method ******");
+
+		return "Hello  " + name + " id: " + id + " Responsed on : " + new Date();
+	}
+
+	public String fallbackMethod(String name, String id, String id1) {
+		System.out.println("******* In fallback method ******");
+		return "fallBackHello";
+	}
+
+}
